bc61d160a9f9b895ee90e1cd5d2c73b5c89ea05b
Manuel J <mrmj1996@gmail.com>
2018-12-10 17:23:17 +0100

    Migration merge fixed

--- decide/voting/migrations/0004_voting_custom_url.py
+++ decide/voting/migrations/0004_voting_custom_url.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('voting', '0003_auto_20180605_0842'),
+        ('voting', '0004_auto_20181210_1127'),
     ]
 
     operations = [
